e1df5ec1571e07aac1ef13c52d043d38f8057d35
Drubo <tareq@webkutir.net>
2011-12-20 13:02:39 +0600

    fixed #2

--- app.rb
+++ app.rb
@@ -27,10 +27,6 @@ helpers do
     settings.token == params[:token]
   end
 
-  def reopen?
-    commit["author"]["name"]==gituser
-  end
-  
   def respond_to_commits
     return "UNKNOWN APP" unless authorized?
     payload["commits"].reverse.each do |commit|
@@ -46,10 +42,8 @@ end
 
 post '/action/:token' do
   respond_to_commits do |commit|
-    puts commit["author"]["name"]
-    puts gituser
-    call env.merge("PATH_INFO" => '/reopen/'+params[:token]) unless reopen?
-    if reopen?
+    call env.merge("PATH_INFO" => '/reopen/'+params[:token]) unless commit["author"]["name"]==gituser
+    if commit["author"]["name"]==gituser
       call env.merge("PATH_INFO" => '/noreopen/'+params[:token])
     end 
   end
