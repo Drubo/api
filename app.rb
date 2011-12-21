08086479e7eb45fa6de2447125bcaf1d5e019fac
Tariqul Islam <tareq@webkutir.net>
2011-12-21 14:25:06 +0600

    check

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ end
 
 get '/' do
   'Api Initialized...'
-  issue_info = Crack::JSON.parse((github.view_issue 52).to_s)
+  issue_info = Crack::JSON.parse((github.view_issue 52).inspect)
 end
 
 post '/action/:token' do
