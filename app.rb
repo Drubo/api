a02c587d6e7bc4a33406b344e9c8d925d53701f1
Tariqul Islam <tareq@webkutir.net>
2011-12-23 11:38:10 +0600

    Direct Check

--- app.rb
+++ app.rb
@@ -52,37 +52,37 @@ end
 post '/action/:token' do
   respond_to_commits do |commit|
     GitHub.closed_issues(commit["message"]) do |issue|
-      call env.merge("PATH_INFO" => '/reopen/'+issue+'/'+commit["id"]+'/'+commit["author"]["name"]) unless commit["author"]["email"]==gitemail
+      call env.merge("PATH_INFO" => '/reopen/#{issue}/#{commit["id"]}/#{commit["author"]["name"]}') unless commit["author"]["email"]==gitemail
       if commit["author"]["email"]==gitemail
-        call env.merge("PATH_INFO" => '/noreopen/'+issue+'/'+commit["author"]["name"])
+        call env.merge("PATH_INFO" => '/noreopen/#{issue}/#{commit["author"]["name"]}')
       end 
     end
   end
 end
 
 post '/reopen/:issue/:commit_id/:commit_author' do
-  call env.merge("PATH_INFO" => '/check_issue_label/'+params[:issue]+'/Accepted')
+  call env.merge("PATH_INFO" => '/check_issue_label/#{params[:issue]}/Accepted')
   return "Issue Accepted" unless found=="false"
 
-  call env.merge("PATH_INFO" => '/check_issue_label/'+params[:issue]+'/New Issue')
+  call env.merge("PATH_INFO" => '/check_issue_label/#{params[:issue]}/New Issue')
   if found=="true"
-    call env.merge("PATH_INFO" => '/re_label_issue/'+params[:issue]+'/'+params[:commit_author])
+    call env.merge("PATH_INFO" => '/re_label_issue/#{params[:issue]}/#{params[:commit_author]}')
     return "Do not Reopen for Review because Code is not Merged yet..."
   end
   if found=="false"
-    call env.merge("PATH_INFO" => '/check_issue_label/'+params[:issue]+'/Re-Opened')
+    call env.merge("PATH_INFO" => '/check_issue_label/#{params[:issue]}/Re-Opened')
     if found=="true"
       github.remove_issue_label params[:issue], "Re-Opened"
       github.add_issue_label params[:issue], "Again"
       return "Again Fixed by Developer"
     end
-    call env.merge("PATH_INFO" => '/check_issue_label/'+params[:issue]+'/Again')
+    call env.merge("PATH_INFO" => '/check_issue_label/#{params[:issue]}/Again')
     if found=="true"
       github.remove_issue_label params[:issue], "Again"
       github.add_issue_label params[:issue], "Re-Opened"
     end
     github.reopen_issue params[:issue]
-    call env.merge("PATH_INFO" => '/comment/'+params[:issue]+'/'+params[:commit_id])
+    call env.merge("PATH_INFO" => '/comment/#{params[:issue]}/#{params[:commit_id]}')
     github.add_issue_label params[:issue], "Waiting For Review"
   end
 end
