9b870d719abdcc3d4906c20c386e31f34f8af120
Tariqul Islam <tareq@webkutir.net>
2011-12-29 11:18:02 +0600

    to view payload

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ helpers do
   
   def respond_to_commits
     return "UNKNOWN APP" unless authorized?
-    puts params[:payload]
+    params[:payload]
     payload["commits"].reverse.each do |commit|
       yield commit
     end
